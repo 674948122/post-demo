6186dcf57b5b2c6a088c9fa801aa3e74d03bb8e9
674948122 <674948122@qq.com>
2017-07-03 09:21:59 +0800

    1
    1

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <leftNav></leftNav>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import leftNav from '@/components/common/leftNav'
+export default {
+  name: 'app',
+  components:{
+    leftNav
+  }
+}
+</script>
+
+<style lang="stylus">
+  .main
+    float left
+    width 95%
+    background #eff2f7
+    height 100%
+    overflow hidden
+</style>
+
+
+
+
